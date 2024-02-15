553e5c98bb7b50b1c3506a735317d47ecd0f6342
Jinglun Jiang <jinglunjiang@gmail.com>
2024-02-15 12:10:06 -0600

    still not working

--- JiangJinglunProject8/src/VirtualMachineTranslator.java
+++ JiangJinglunProject8/src/VirtualMachineTranslator.java
@@ -8,6 +8,10 @@ public class VirtualMachineTranslator{
 
   private static final String[] MEMORY_ACCESS_COMMANDS = {"push", "pop"};
 
+  private static final String[] PROGRAM_FLOW_COMMANDS = {"label", "goto", "if-goto"};
+
+  private static final String[] FUNCTION_CALLING_COMMANDS = {"function", "call", "return"};
+
   private static final String[] SEGMENT_MAP = {"argument", "local", "this", "that"};
 
   private static final String[] TEMP_SEGMENTS = {"0", "1", "2", "3", "4", "5", "6", "7"};
@@ -19,6 +23,8 @@ public class VirtualMachineTranslator{
   private static final String CONSTANT_SEGMENT = "constant";
 
   private static int labelCounter = 0;
+
+  private static int returnCounter = 0;
 
   /* 
     Main function that takes in the input file
@@ -32,16 +38,69 @@ public class VirtualMachineTranslator{
     }
 
     String inputFileName = args[0];
-    String outputFileName = inputFileName.replace(".vm", ".asm");
+    File inputFile = new File(inputFileName);
 
-    try{
+    if (inputFile.isFile()){
+      translateFile(inputFileName);
+    } else if (inputFile.isDirectory()){
+      translateDirectory(inputFile);
+    } else{
+      System.err.println("Invalid input: " + inputFileName);
+    }
+
+    // String inputFileName = args[0];
+    // String outputFileName = inputFileName.replace(".vm", ".asm");
+
+    // try{
+    //   List<String> commands = Files.readAllLines(Paths.get(inputFileName));
+    //   List<String> translatedCommands = translateVMtoASM(commands);
+
+    //   String bootstrapCode = generateBoostrapCode();
+    //   translatedCommands.add(0, bootstrapCode);
+
+    //   writeLinesToFile(outputFileName, translatedCommands);
+
+    //   System.out.println("Translation completed. Output file: " + outputFileName);
+    // } catch (IOException e){
+    //   System.err.println("Error reading input file: " + e.getMessage());
+    // }
+  }
+
+  private static void translateFile(String inputFileName) {
+    String outputFileName = inputFileName.replace(".vm", ".asm");
+    String fileNameWithoutExtension = new File(inputFileName).getName().replaceFirst("[.][^.]+$", "");
+
+    try {
       List<String> commands = Files.readAllLines(Paths.get(inputFileName));
-      List<String> translatedCommands = translateVMtoASM(commands);
+      List<String> translatedCommands = translateVMtoASM(commands, fileNameWithoutExtension);
+
       writeLinesToFile(outputFileName, translatedCommands);
+
       System.out.println("Translation completed. Output file: " + outputFileName);
-    } catch (IOException e){
+    } catch (IOException e) {
       System.err.println("Error reading input file: " + e.getMessage());
     }
+  }
+  
+  private static void writeLinesToFile(String fileName, List<String> lines){
+    try(FileWriter writer = new FileWriter(fileName)){
+      for (String line : lines){
+        writer.write(line);
+      }
+    } catch(IOException e){
+      System.err.println("Error writing to file: " + e.getMessage());
+    }
+  }
+
+  private static String generateBoostrapCode(){
+    StringBuilder bootstrapCode = new StringBuilder();
+    bootstrapCode.append("// Bootstrap code\n");
+    bootstrapCode.append("@256\n");
+    bootstrapCode.append("D=A\n");
+    bootstrapCode.append("@SP\n");
+    bootstrapCode.append("M=D\n");
+    bootstrapCode.append(functionCall("Sys.init", "0"));
+    return bootstrapCode.toString();
   }
 
   /*
@@ -63,9 +122,13 @@ public class VirtualMachineTranslator{
     For each of the command, check if it is an arithmetic command or memory access command
     Write an infinite loop at the end of the file
   */
-  private static List<String> translateVMtoASM(List<String> commands){
+  private static List<String> translateVMtoASM(List<String> commands, String fileName){
     List<String> translatedCommands = new ArrayList<>();
     for (String command : commands){
+      command = command.trim();
+      if (command.isEmpty() || command.startsWith("//")) {
+        continue;
+      }
       String[] parts = command.split("\\s+");
       if (parts.length == 0){
         continue;
@@ -75,6 +138,12 @@ public class VirtualMachineTranslator{
       }
       else if (isMemoryAccessCommand(parts[0])){
         translatedCommands.add(translateMemoryAccessCommand(parts));
+      }
+      else if (isProgramFlowCommand(parts[0])){
+        translatedCommands.add(translateProgramFlowCommand(parts));
+      }
+      else if (isFunctionCallingCommand(parts[0])){
+        translatedCommands.add(translateFunctionCallingCommand(parts));
       }
     }
     translatedCommands.add("(END_LOOP)\n");
@@ -110,6 +179,24 @@ public class VirtualMachineTranslator{
     return false;
   }
 
+  private static boolean isProgramFlowCommand(String command){
+    for (String cmd : PROGRAM_FLOW_COMMANDS){
+      if (cmd.equals(command)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static boolean isFunctionCallingCommand(String command){
+    for (String cmd : FUNCTION_CALLING_COMMANDS){
+      if (cmd.equals(command)){
+        return true;
+      }
+    }
+    return false;
+  }
+
   /*
     Handles the arithmetic command
   */
@@ -122,7 +209,7 @@ public class VirtualMachineTranslator{
         asmCode.append("AM=M-1\n");
         asmCode.append("D=M\n");
         asmCode.append("A=A-1\n");
-        asmCode.append("M=M+D\n");
+        asmCode.append("M=D+M\n");
         break;
       case "sub":
         asmCode.append("// sub\n");
@@ -373,4 +460,204 @@ public class VirtualMachineTranslator{
     }
     return asmCode.toString();
   }
+
+  private static String translateProgramFlowCommand(String[] parts){
+    StringBuilder asmCode = new StringBuilder();
+    switch (parts[0]){
+      case "label":
+        asmCode.append("(" + parts[1] + ")\n");
+        break;
+      case "goto":
+        asmCode.append("@" + parts[1] + "\n");
+        asmCode.append("0;JMP\n");
+        break;
+      case "if-goto":
+        asmCode.append("@SP\n");
+        asmCode.append("AM=M-1\n");
+        asmCode.append("D=M\n");
+        asmCode.append("@" + parts[1] + "\n");
+        asmCode.append("D;JNE\n");
+        break;
+    }
+    return asmCode.toString();
+  }
+
+  private static String translateFunctionCallingCommand(String[] parts){
+    StringBuilder asmCode = new StringBuilder();
+    switch (parts[0]){
+      case "function":
+        if (parts[2].equals("0")){
+          asmCode.append("(" + parts[1] + ")\n");
+        }
+        else{
+          return functionInitialization(parts[1], parts[2]);
+        }
+        break;
+      case "call":
+        return functionCall(parts[1], parts[2]);
+      case "return":
+        return functionReturn();
+    }
+    return asmCode.toString();
+  }
+
+  private static String functionInitialization(String functionName, String numberOfVariable){
+    StringBuilder asmCode = new StringBuilder();
+    String label = "Initialize" + functionName + numberOfVariable;
+    asmCode.append("(" + functionName + ")\n");
+    asmCode.append("\n//Initialize function\n");
+    asmCode.append("@" + numberOfVariable + "\n");
+    asmCode.append("D=A\n");
+    asmCode.append("(" + label + ")\n");
+    asmCode.append("@SP\n");
+    asmCode.append("A=M\n");
+    asmCode.append("M=0\n");
+    asmCode.append("@SP\n");
+    asmCode.append("M=M+1\n");
+    asmCode.append("D=D-1\n");
+    asmCode.append("@" + label + "\n");
+    asmCode.append("D;JNE\n");
+    return asmCode.toString();
+  }
+
+  private static String functionCall(String functionName, String numberOfVariable){
+    StringBuilder asmCode = new StringBuilder();
+    String returnAddress = "RETURN_ADDRESS_" + Integer.toString(returnCounter);
+    returnCounter++;
+    
+    //push return address
+    asmCode.append("\n//push return address\n");
+    asmCode.append("@" + returnAddress + "\n");
+    asmCode.append("D=A\n");
+    asmCode.append("@SP\n");
+    asmCode.append("A=M\n");
+    asmCode.append("M=D\n");
+    asmCode.append("@SP\n");
+    asmCode.append("M=M+1\n");
+
+    //push LCL, ARG, THIS, THAT
+    String[] memorySegmentList = {"LCL", "ARG", "THIS", "THAT"};
+    for (String memorySegment : memorySegmentList){
+      asmCode.append("\n// push" + memorySegment + "\n");
+      asmCode.append("@" + memorySegment + "\n");
+      asmCode.append("D=M\n");
+      asmCode.append("@SP\n");
+      asmCode.append("AM=M+1\n");
+      asmCode.append("A=A-1\n");
+      asmCode.append("M=D\n");
+    }
+
+    //ARG = SP - n - 5
+    asmCode.append("\n// ARG = SP - n - 5\n");
+    asmCode.append("@SP\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@5\n");
+    asmCode.append("D=D-A\n");
+    asmCode.append("@" + numberOfVariable + "\n");
+    asmCode.append("D=D-A\n");
+    asmCode.append("@ARG\n");
+    asmCode.append("M=D\n");
+
+    //LCL = SP
+    asmCode.append("\n// LCL = SP\n");
+    asmCode.append("@SP\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@LCL\n");
+    asmCode.append("M=D\n");
+
+    //goto f
+    asmCode.append("\n// goto f\n");
+    asmCode.append("@" + functionName + "\n");
+    asmCode.append("0;JMP\n");
+
+    //set return address
+    asmCode.append("\n// set return address\n");
+    asmCode.append("(" + returnAddress + ")\n");
+    asmCode.append("0;JMP\n");
+
+    return asmCode.toString();
+  }
+
+  private static String functionReturn(){
+    StringBuilder asmCode = new StringBuilder();
+    //FRAME = LCL
+    asmCode.append("// FRAME = LCL\n");
+    asmCode.append("@LCL\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("M=D\n");
+
+    //RET = *(FRAME - 5)
+    asmCode.append("\n// RET = *(FRAME - 5)\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@5\n");
+    asmCode.append("A=D-A\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@RET\n");
+    asmCode.append("M=D\n");
+
+    //*ARG = pop()
+    asmCode.append("\n// *ARG = pop()\n");
+    asmCode.append("@SP\n");
+    asmCode.append("AM=M-1\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@ARG\n");
+    asmCode.append("A=M\n");
+    asmCode.append("M=D\n");
+
+    //SP = ARG + 1
+    asmCode.append("\n// SP = ARG + 1\n");
+    asmCode.append("@ARG\n");
+    asmCode.append("D=M+1\n");
+    asmCode.append("@SP\n");
+    asmCode.append("M=D\n");
+
+    //THAT = *(FRAME - 1)
+    asmCode.append("\n// THAT = *(FRAME - 1)\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("A=M-1\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@THAT\n");
+    asmCode.append("M=D\n");
+
+    //THIS = *(FRAME - 2)
+    asmCode.append("\n// THAT = *(FRAME - 1)\n");
+    asmCode.append("@2\n");
+    asmCode.append("D=A\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("A=M-D\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@THIS\n");
+    asmCode.append("M=D\n");
+
+    //ARG = *(FRAME - 3)
+    asmCode.append("\n// ARG = *(FRAME - 3)\n");
+    asmCode.append("@3\n");
+    asmCode.append("D=A\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("A=M-D\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@ARG\n");
+    asmCode.append("M=D\n");
+
+    //LCL = *(FRAME - 4)
+    asmCode.append("\n// LCL = *(FRAME - 4)\n");
+    asmCode.append("@4\n");
+    asmCode.append("D=A\n");
+    asmCode.append("@FRAME\n");
+    asmCode.append("A=M-D\n");
+    asmCode.append("D=M\n");
+    asmCode.append("@LCL\n");
+    asmCode.append("M=D\n");
+
+    //goto RET
+    asmCode.append("\n// goto RET\n");
+    asmCode.append("@RET\n");
+    asmCode.append("A=M\n");
+    asmCode.append("0;JMP\n");
+
+    return asmCode.toString();
+  }
+
 }
